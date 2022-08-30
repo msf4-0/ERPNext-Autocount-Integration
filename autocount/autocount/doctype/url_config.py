cf9838f18d995765ebd2fbb1c48fb3b7b28b4b57
Tim <twying1702@gmail.com>
2022-08-31 02:33:56 +0800

    Update url_config

--- autocount/autocount/doctype/url_config.py
+++ autocount/autocount/doctype/url_config.py
@@ -36,11 +36,11 @@ def get_socket_address():
 	port = data.get("port")
 	if not ip_address:
 		ip_address = DEFAULT_IP_ADDRESS
-		frappe.db.set("Autocount Settings", "Autocount Settings", "ip_address", DEFAULT_IP_ADDRESS)
+		frappe.db.set_value("Autocount Settings", "Autocount Settings", "ip_address", DEFAULT_IP_ADDRESS)
 		frappe.db.commit()
 	if not port:
 		port = DEFAULT_PORT
-		frappe.db.set("Autocount Settings", "Autocount Settings", "port", DEFAULT_PORT)
+		frappe.db.set_value("Autocount Settings", "Autocount Settings", "port", DEFAULT_PORT)
 		frappe.db.commit()
 	return f"http://{ip_address}:{port}"
 
